0fb0833fcd62e4b5653eec0226a4c5a2d027b497
Meisterbogen21 <setogw1212@gmail.com>
2025-01-10 13:57:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def tampilkan_data_sewa():
             data = list(reader)
             if len(data) > 1:
                 st.write("### Data Sewa Mobil")
-                st.table(data[1:])  # Menampilkan data tanpa header
+                st.table(pd.DataFrame(data[0:], columns = ["Penyewa", "No. KTP", "No. Telp", "Jenis Mobil",  "Tanggal Sewa", "Tanggal Kembali"]))  # Menampilkan data tanpa header
             else:
                 st.info("Tidak ada data sewa.")
     except FileNotFoundError:
