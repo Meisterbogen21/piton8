77b4abab6a900bad7f32c66463a7f25a44c3cbe1
Meisterbogen21 <setogw1212@gmail.com>
2025-01-10 13:51:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def tampilkan_data_sewa():
             data = list(reader)
             if len(data) > 1:
                 st.write("### Data Sewa Mobil")
-                st.table(data[0:])  # Menampilkan data tanpa header
+                st.table(data[1:])  # Menampilkan data tanpa header
             else:
                 st.info("Tidak ada data sewa.")
     except FileNotFoundError:
